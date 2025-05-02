a853585273fa9d6d4302c88180415194c729961b
gmlghd98 <gmlghd98@gmail.com>
2025-05-02 16:04:20 +0900

    Define Method of Controller & Service (Sign)

--- src/main/java/com/webkit/sonsation_server/controller/SignController.java
+++ src/main/java/com/webkit/sonsation_server/controller/SignController.java
@@ -1,4 +1,42 @@
 package com.webkit.sonsation_server.controller;
 
+import com.webkit.sonsation_server.mapper.SignDetail;
+import com.webkit.sonsation_server.mapper.SignListDetail;
+import com.webkit.sonsation_server.mapper.SignListItem;
+import com.webkit.sonsation_server.response.ApiResponse;
+import com.webkit.sonsation_server.service.SignService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/sign")
 public class SignController {
+    private final SignService signService;
+
+    @GetMapping
+    public ApiResponse<List<SignListItem>> getAllSigns(){
+        return signService.getAllSigns();
+    }
+
+    @GetMapping
+    public ApiResponse<List<SignListDetail>> searchSigns(
+            @RequestParam(value = "keyword", defaultValue = "") String keyword
+    ){
+        return signService.searchSigns(keyword);
+    }
+
+    @GetMapping("/all")
+    public ApiResponse<List<String>> getAllSignNames(){
+        return signService.getAllSignNames();
+    }
+
+    @GetMapping("/{sign_id}")
+    public ApiResponse<SignDetail> getSign(
+            @PathVariable(value = "sign_id") Long id
+    ){
+        return signService.getSign(id);
+    }
 }
---
+++ src/main/java/com/webkit/sonsation_server/repository/SignRepository.java
@@ -0,0 +1,8 @@
+package com.webkit.sonsation_server.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface SignRepository extends JpaRepository {
+
+}
